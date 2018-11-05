6fa6ee8bad6fca6b072c9299e5825cadb08e7a86
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-11-05 11:10:37 +0100

    Update masking

--- simpleference/masking/prediction_blocks_from_mask.py
+++ simpleference/masking/prediction_blocks_from_mask.py
@@ -6,29 +6,43 @@ import h5py
 import numpy as np
 
 
-def make_prediction_blocks(full_shape, downscale_factor, output_shape, mask_file, output_file):
+def make_prediction_blocks(full_shape, output_shape, mask_file, output_file,
+                           downscale_factor=None, roi_begin=None, roi_end=None, mask_key='data'):
 
-    output_shape_ds = [outs / ds for outs, ds in zip(output_shape, downscale_factor)]
-
-    pred_mask_shape = [sfull // outs + 1 for sfull, outs in zip(full_shape, output_shape)]
+    if roi_begin is not None:
+        assert roi_end is not None
+        roi_begin_c = [(rois // outs) * outs for rois, outs in zip(roi_begin, output_shape)]
+        roi_end_c = [(rois // outs + 1) * outs for rois, outs in zip(roi_end, output_shape)]
+    else:
+        roi_begin = [0, 0, 0]
+        roi_end = full_shape
 
     assert os.path.exists(mask_file), mask_file
     with h5py.File(mask_file) as f:
-        ds = f['data']
-        mask = ds[:]
+        mask = f[mask_key][:]
+        print(np.sum(mask))
 
-    print("Making prediction blocks for full shape", full_shape)
-    print("Downscaled shape:", tuple(fs // ds for fs, ds in zip(full_shape, downscale_factor)))
-    print("From mask shape :", mask.shape)
-    print("(these should more or less agree !)")
+    if downscale_factor is None:
+        print("Determining downscale factor from shapes")
+        mask_shape = mask.shape
+        downscale_factor = tuple(fs / ms for fs, ms in zip(full_shape, mask_shape))
+        print(downscale_factor)
+    else:
+        print("Making prediction blocks for full shape", full_shape)
+        print("Downscaled shape:", tuple(fs // ds for fs, ds in zip(full_shape, downscale_factor)))
+        print("From mask shape :", mask.shape)
+        print("(these should more or less agree !)")
+
+    output_shape_ds = [outs / ds for outs, ds in zip(output_shape, downscale_factor)]
+    pred_mask_shape = [sfull // outs + 1 for sfull, outs in zip(full_shape, output_shape)]
 
     prediction_mask = np.zeros(pred_mask_shape, dtype='uint8')
     prediction_blocks = []
     # generate blocks
-    for z in range(0, full_shape[0], output_shape[0]):
-        print("generating for", z, "/", full_shape[0])
-        for y in range(0, full_shape[1], output_shape[1]):
-            for x in range(0, full_shape[2], output_shape[2]):
+    for z in range(roi_begin[0], roi_end[0], output_shape[0]):
+        print("generating for", z, "/", roi_end[0])
+        for y in range(roi_begin[1], roi_end[1], output_shape[1]):
+            for x in range(roi_begin[2], roi_end[2], output_shape[2]):
                 z_ds = z / downscale_factor[0]
                 y_ds = y / downscale_factor[1]
                 x_ds = x / downscale_factor[2]
@@ -44,6 +58,8 @@ def make_prediction_blocks(full_shape, downscale_factor, output_shape, mask_file
                 ]
 
                 mask_block = mask[bb]
+                # print(bb)
+                # print(mask_block.shape)
 
                 if np.sum(mask_block) > 0:
                     prediction_blocks.append([z, y, x])
