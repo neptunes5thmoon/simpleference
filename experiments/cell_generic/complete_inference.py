8a53071e3ad240913e2687147fd1fc6f5e772ae7
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2019-02-27 11:22:13 -0500

    flexible cell organelle inference

---
+++ experiments/cell_generic/complete_inference.py
@@ -0,0 +1,148 @@
+from __future__ import print_function
+import sys
+import os
+import json
+from concurrent.futures import ProcessPoolExecutor
+
+from subprocess import call
+sys.path.append('/groups/saalfeld/home/heinrichl/tmp/simpleference')
+sys.path.append('/groups/saalfeld/home/heinrichl/Projects/CNNectome')
+sys.path.append('/groups/saalfeld/home/heinrichl/Projects/git_repos/gunpowder')
+sys.path.append('/groups/saalfeld/home/papec/Work/my_projects/z5/bld27/python')
+from simpleference.inference.util import get_offset_lists, redistribute_offset_lists
+from utils.label import Label
+#from simpleference.inference.util import offset_list_from_precomputed
+import z5py
+import numpy as np
+
+#from precompute_offsets import precompute_offset_list
+
+
+def single_inference(gpu, iteration, path, out_file, scale, shift):
+    call(['./run_inference.sh', str(gpu),
+          str(iteration), str(path), str(out_file), str(scale), str(shift)])
+    return True
+
+
+def complete_inference(path, min_sc, max_sc, out_file, gpu_list, iteration, labels, compute_offset_lists=True):
+    #path = '/groups/saalfeld/saalfeldlab/projects/cell/nrs-data/cell2/test2.n5'
+    #path = '/groups/saalfeld/saalfeldlab/larissa/data/cell/test_cell2_v1.n5'
+    assert os.path.exists(path), "Path to N5 dataset with raw data and mask does not exist"
+    rf = z5py.File(path, use_zarr_format=False)
+    assert 'volumes/raw' in rf, "Raw data not present in N5 dataset"
+    #assert 'volumes/orig_raw' in rf, "Raw data not present in N5 dataset"
+    shape = rf['volumes/raw'].shape
+    #shape = rf['volumes/orig_raw'].shape
+
+    # create the datasets
+    output_shape = (236, 236, 236)
+    #out_file = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/test2_{0:}.n5'.format(iteration)
+    #out_file ='/nrs/saalfeld/heinrichl/cell/gt110618/setup03/run01/test_cell2_v1_{0:}.n5'.format(iteration)
+    if not os.path.exists(out_file):
+        os.mkdir(out_file)
+
+    f = z5py.File(out_file, use_zarr_format=False)
+    # the n5 datasets might exist already
+    if compute_offset_lists:
+        for label in labels:
+            f.create_dataset(label.labelname, shape=shape, compression='gzip', dtype='uint8', chunks=output_shape)
+    # make the offset files, that assign blocks to gpus
+    # generate offset lists with mask
+    if compute_offset_lists:
+        get_offset_lists(shape, gpu_list, out_file, output_shape=output_shape)
+    else:
+        redistribute_offset_lists(gpu_list, out_file)
+    # run multiprocessed inference
+    scale = 1./(max_sc - min_sc)
+    shift = - min_sc * scale
+    with ProcessPoolExecutor(max_workers=len(gpu_list)) as pp:
+        tasks = [pp.submit(single_inference, gpu, iteration, path, out_file, scale, shift) for gpu in gpu_list]
+        result = [t.result() for t in tasks]
+
+    if all(result):
+        print("All gpu's finished inference properly.")
+    else:
+        print("WARNING: at least one process didn't finish properly.")
+    re_gpu_list = []
+    for gpu in gpu_list:
+        if not check_completeness(out_file, gpu):
+            re_gpu_list.append(gpu)
+    if len(re_gpu_list) > 0:
+        complete_inference(path, min_sc, max_sc, out_file, re_gpu_list, iteration, labels, compute_offset_lists=False)
+
+
+def check_completeness(out_file, gpu):
+    if os.path.exists(os.path.join(out_file, 'list_gpu_{0:}.json'.format(gpu))) and os.path.exists(
+            os.path.join(out_file, 'list_gpu_{0:}_{1:}_processed.txt'.format(gpu, iteration))):
+        block_list = os.path.join(out_file, 'list_gpu_{0:}.json'.format(gpu))
+        block_list_processed = os.path.join(out_file, 'list_gpu_{0:}_{1:}_processed.txt'.format(gpu, iteration))
+        with open(block_list, 'r') as f:
+            block_list = json.load(f)
+            block_list = {tuple(coo) for coo in block_list}
+        with open(block_list_processed, 'r') as f:
+            list_as_str = f.read()
+        list_as_str_curated = '[' + list_as_str[:list_as_str.rfind(']') + 1] + ']'
+        processed_list = json.loads(list_as_str_curated)
+        processed_list = {tuple(coo) for coo in processed_list}
+        if processed_list < block_list:
+            complete = False
+        else:
+            complete = True
+    else:
+        complete = False
+    return complete
+
+if __name__ == '__main__':
+    gpu_list = [4, 5, 6, 7]
+    iteration = 100000
+    path = '/groups/saalfeld/saalfeldlab/projects/cell/nrs-data/cell2/test2.n5'
+    out_path = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/test2_{0:}.n5'.format(iteration)
+    min_sc = 0.
+    max_sc = 1.
+    # path = '/groups/hess/hess_collaborators/Annotations/ParentFiles_whole-cell_images/TWalther_WT45_Cell2_4x4x4nm' \
+    #       '/Cryo_20171009_WT45_Cell2_4x4x4nm.n5'
+    #out_path = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/walther_{0:}.n5'.format(iteration)
+    #min_sc = 0.57
+    #max_sc = 0.99
+    # path = '/groups/hess/hess_collaborators/Annotations/ParentFiles_whole-cell_images/Chlamydomonas' \
+    #        '/Chlamydomonas_4x4x4nm.n5'
+    # out_path = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/chlamydomonas_{0:}.n5'.format(iteration)
+    # min_sc = 0.3
+    # max_sc=1.
+    # path = '/groups/hess/hess_collaborators/Annotations/ParentFiles_whole-cell_images/Macrophage_FS80_Cell2_4x4x4nm' \
+    #         '/Cryo_FS80_Cell2_4x4x4nm.n5'
+    # out_path = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/macrophage_{0:}.n5'.format(iteration)
+    # min_sc = 0.71
+    # max_sc=1.
+    #BEFORE STARTING A NEW PREDICTION CHANGE SCALING
+    #path = '/groups/hess/hess_collaborators/Annotations/ParentFiles_whole-cell_images/HeLa_Cell3_4x4x4nm/Aubrey_17-7_17_Cell3%204x4x4nm.n5'
+    #out_path = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/cell3_{0:}.n5'.format(iteration)
+    #min_sc = -0.55
+    #max_sc=1.64
+
+    labels = []
+    labels.append(Label('ecs', 1))
+    labels.append(Label('plasma_membrane', 2))
+    labels.append(Label('mito', (3, 4, 5)))
+    labels.append(Label('mito_membrane', 3, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('mito_DNA', 5, scale_loss=False, scale_key=labels[-2].scale_key))
+    labels.append(Label('vesicle', (8, 9)))
+    labels.append(Label('vesicle_membrane', 8, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('MVB', (10, 11)))
+    labels.append(Label('MVB_membrane', 10, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('lysosome', (12, 13)))
+    labels.append(Label('lysosome_membrane', 12, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('LD', (14, 15)))
+    labels.append(Label('LD_membrane', 14, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('er', (16, 17, 18, 19, 20, 21)))
+    labels.append(Label('er_membrane', (16, 18, 20), scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('ERES', (18, 19)))
+    labels.append(Label('ERES_membrane', 18, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('nucleus', (20,21,24,25)))
+    labels.append(Label('NE', (20, 21), scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('NE_membrane', 20, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('chromatin', 24, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('nucleoplasm', 25, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('microtubules', 26))
+    labels.append(Label('ribosomes', 1))
+    complete_inference(path, min_sc, max_sc, out_path, gpu_list, iteration, labels, compute_offset_lists=True)
---
+++ experiments/cell_generic/run_inference.py
@@ -0,0 +1,106 @@
+import os
+import sys
+sys.path.append('/groups/saalfeld/home/papec/Work/my_projects/z5/bld27/python')
+sys.path.append('/groups/saalfeld/home/heinrichl/Projects/CNNectome')
+import time
+import json
+import z5py
+from functools import partial
+from utils.label import Label
+from simpleference.inference.inference import run_inference_n5
+from simpleference.backends.gunpowder.tensorflow.backend import TensorflowPredict
+from simpleference.backends.gunpowder.preprocess import zero_out_const_sections,clip,scale_shift,normalize
+from simpleference.postprocessing import *
+
+def clip_preprocess(data, scale=2, shift=-1):
+    return zero_out_const_sections(clip(scale_shift(normalize(data),
+                                                         scale,
+                                                         shift)))
+def single_gpu_inference(gpu, iteration, labels, path, out_file, scale=2., shift=1.):
+
+  #  path = '/groups/saalfeld/saalfeldlab/projects/cell/nrs-data/cell2/test2.n5'
+    #path = '/groups/saalfeld/saalfeldlab/larissa/data/cell/test_cell2_v1.n5'
+    assert os.path.exists(path), path
+    rf = z5py.File(path, use_zarr_format=False)
+    shape = rf['volumes/raw'].shape
+    #shape = rf['volumes/orig_raw'].shape
+    weight_meta_graph = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/unet_checkpoint_%i' % iteration
+    inference_meta_graph = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/unet_inference'
+    net_io_json = '/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/net_io_names.json'
+
+   # out_file ='/nrs/saalfeld/heinrichl/cell/gt122018/setup01/run02/test2_{0:}.n5'.format(iteration)
+    #out_file ='/nrs/saalfeld/heinrichl/cell/gt110618/setup03/run01/test_cell2_v1_{0:}.n5'.format(iteration)
+    with open(net_io_json, 'r') as f:
+        net_io_names = json.load(f)
+
+    offset_file = out_file+'/list_gpu_{0:}.json'.format(gpu)
+    with open(offset_file, 'r') as f:
+        offset_list = json.load(f)
+
+    input_key = net_io_names["raw"]
+    output_keys=[]
+    target_keys=[]
+    for label in labels:
+        output_keys.append(net_io_names[label.labelname])
+        target_keys.append(label.labelname)
+    input_shape = (340, 340, 340)
+    output_shape = (236, 236, 236)
+    prediction = TensorflowPredict(weight_meta_graph,
+                                   inference_meta_graph,
+                                   input_key=input_key,
+                                   output_key=output_keys)
+    t_predict = time.time()
+    run_inference_n5(prediction,
+                     partial(clip_preprocess, scale=scale*2., shift=shift*2.-1.),
+                     partial(clip_float_to_uint8, float_range=(-1., 1.), safe_scale=False),
+                     path,
+                     out_file,
+                     offset_list,
+                     input_shape=input_shape,
+                     output_shape=output_shape,
+                     target_keys= target_keys,
+                   #  input_key='volumes/orig_raw',
+                     input_key='volumes/raw',
+                     log_processed=os.path.join(os.path.dirname(offset_file), 'list_gpu_{0:}_{'
+                                                                                '1:}_processed.txt'.format(gpu,
+                                                                                                           iteration)))
+
+    t_predict = time.time() - t_predict
+
+    with open(os.path.join(os.path.dirname(offset_file), 't-inf_gpu_{0:}_{1:}.txt'.format(gpu, iteration)), 'w') as f:
+        f.write("Inference with gpu %i in %f s\n" % (gpu, t_predict))
+
+
+if __name__ == '__main__':
+    gpu = int(sys.argv[1])
+    iteration = int(sys.argv[2])
+    path = str(sys.argv[3])
+    out_file = str(sys.argv[4])
+    scale = float(sys.argv[5])
+    shift = float(sys.argv[6])
+    labels = []
+    labels.append(Label('ecs', 1))
+    labels.append(Label('plasma_membrane', 2))
+    labels.append(Label('mito', (3, 4, 5)))
+    labels.append(Label('mito_membrane', 3, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('mito_DNA', 5, scale_loss=False, scale_key=labels[-2].scale_key))
+    labels.append(Label('vesicle', (8, 9)))
+    labels.append(Label('vesicle_membrane', 8, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('MVB', (10, 11)))
+    labels.append(Label('MVB_membrane', 10, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('lysosome', (12, 13)))
+    labels.append(Label('lysosome_membrane', 12, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('LD', (14, 15)))
+    labels.append(Label('LD_membrane', 14, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('er', (16, 17, 18, 19, 20, 21)))
+    labels.append(Label('er_membrane', (16, 18, 20), scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('ERES', (18, 19)))
+    labels.append(Label('ERES_membrane', 18, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('nucleus', (20,21,24,25)))
+    labels.append(Label('NE', (20, 21), scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('NE_membrane', 20, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('chromatin', 24, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('nucleoplasm', 25, scale_loss=False, scale_key=labels[-1].scale_key))
+    labels.append(Label('microtubules', 26))
+    labels.append(Label('ribosomes', 1))
+    single_gpu_inference(gpu, iteration, labels, path, out_file,  scale, shift)
