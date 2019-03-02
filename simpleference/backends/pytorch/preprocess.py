47de2ba5896fb3a6fae666b7d6cd809f0f5ab2af
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-03-02 11:52:58 +0100

    Update pytorch preprocessing

--- simpleference/backends/pytorch/preprocess.py
+++ simpleference/backends/pytorch/preprocess.py
@@ -1,9 +1,11 @@
-import numpy as np
 
-
-def normalize(data, eps=1e-4, mean=None, std=None):
-    mean = data.mean() if mean is None else mean
-    std = data.std() if std is None else std
+def normalize(data, eps=1e-4, mean=None, std=None, filter_zeros=True):
+    if filter_zeros:
+        data_pre = data[data != 0]
+    else:
+        data_pre = data
+    mean = data_pre.mean() if mean is None else mean
+    std = data_pre.std() if std is None else std
     return (data - mean) / (std + eps)
 
 
